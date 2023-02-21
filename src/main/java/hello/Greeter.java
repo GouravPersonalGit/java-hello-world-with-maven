32e4a289e8c5d85c575ce32f9923f3ad23efbe86
GouravPersonalGit <gourav.chd7@gmail.com>
2023-02-21 16:02:03 +0530

    commit for fork project

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,8 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project for AchiStar Technologies";
+        System.out.println("hello");
+    	return "DevOps Project for AchiStar Technologies";
+        
     }
 }
